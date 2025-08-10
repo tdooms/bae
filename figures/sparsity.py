0f2a9060468745a7f43b5bae835c13cd17135474
thomas <thomas@dooms.eu>
2025-08-10 22:38:46 +0200

    oops 3

--- figures/sparsity.py
+++ figures/sparsity.py
@@ -30,32 +30,32 @@ dataset = dataset.map(tokenize, batched=True)
 max_steps = 2**2
 hoyer, l0 = [], []
 
-# coder = Autoencoder.load(model, "ordered", layer=18, expansion=16, alpha=1.0, tags=[], root="//wsl.localhost/Ubuntu/home/thomas/bae/weights").eval().half()
-coder = Autoencoder.load(model, "vanilla", layer=18, expansion=16, alpha=1.0, tags=[], root="//wsl.localhost/Ubuntu/home/thomas/bae/weights").eval().half()
-
 # coder = Autoencoder.load(model, "ordinary", layer=18, expansion=16, alpha=0.1, tags=[]).eval().half()
 # coder = Autoencoder.load(model, "mixed", layer=18, expansion=16, alpha=1.5, tags=[]).eval().half()
 
-loader = DataLoader(dataset, batch_size=32, shuffle=False)
-acts = []
+for kind in ["vanilla", "ordered"]:
+    coder = Autoencoder.load(model, kind, layer=18, expansion=16, alpha=1.0, tags=[], root="//wsl.localhost/Ubuntu/home/thomas/bae/weights").eval().half()
 
-for batch, _ in zip(loader, range(max_steps)):
-    batch = {k: v.cuda() for k, v in batch.items() if isinstance(v, torch.Tensor)}
-    acts += [coder(**batch)['features'].half()]
+    loader = DataLoader(dataset, batch_size=32, shuffle=False)
+    acts = []
 
-acts = torch.cat(acts, dim=0)
-hoyer = inv_hoyer(acts.flatten(0, -2), dim=0)
-l0 = (acts.flatten(0, -2).abs() > 0.01).float().mean(dim=0)
-df = pd.DataFrame.from_dict(dict(hoyer=hoyer.cpu(), l0=l0.cpu()))
-# %%
-fig = px.scatter(df, x=df.index, y="hoyer", opacity=0.3, marginal_y="histogram", template="plotly_white")
-fig.update_yaxes(title="<b>Hoyer</b>", range=(0, 1.005)).update_xaxes(title="<b>Latent index</b>", range=(0, 1024*16), row=1, col=1, showgrid=False)
-fig.update_layout(yaxis2=dict(title=""), xaxis2=dict(title=""))
-fig.update_layout(margin=dict(t=10, b=10, l=10, r=10), width=500, height=300)
-fig.update_layout(font=FONT)
+    for batch, _ in zip(loader, range(max_steps)):
+        batch = {k: v.cuda() for k, v in batch.items() if isinstance(v, torch.Tensor)}
+        acts += [coder(**batch)['features'].half()]
 
-fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False, row=1, col=2).update_yaxes(showticklabels=False, showgrid=False, zeroline=False, row=1, col=2)
-# %%
-fig.write_image("C:/Users/thoma/Downloads/sparsity.svg")
+    acts = torch.cat(acts, dim=0)
+    hoyer = inv_hoyer(acts.flatten(0, -2), dim=0)
+    l0 = (acts.flatten(0, -2).abs() > 0.01).float().mean(dim=0)
+    df = pd.DataFrame.from_dict(dict(hoyer=hoyer.cpu(), l0=l0.cpu()))
+
+    fig = px.scatter(df, x=df.index, y="hoyer", opacity=0.3, marginal_y="histogram", template="plotly_white")
+    fig.update_yaxes(title="<b>Sparsity</b>", range=(0, 1.005)).update_xaxes(title="<b>Latent index</b>", range=(0, 1024*16), row=1, col=1, showgrid=False)
+    fig.update_layout(yaxis2=dict(title=""), xaxis2=dict(title=""))
+    fig.update_layout(margin=dict(t=10, b=10, l=10, r=10), width=500, height=300)
+    fig.update_layout(font=FONT)
+    fig.update_xaxes(showticklabels=False, showgrid=False, zeroline=False, row=1, col=2).update_yaxes(showticklabels=False, showgrid=False, zeroline=False, row=1, col=2)
+    fig.show()
+
+    fig.write_image("C:/Users/thoma/Downloads/sparsity.svg")
 # %%
     
